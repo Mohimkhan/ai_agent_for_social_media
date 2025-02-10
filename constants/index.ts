2ddcbc70bcef5dbf405613cefd102e8be15b83a0
Mohimkhan <mohimkhan652@gmail.com>
2025-02-10 20:07:18 +0600

    chore: add database name constant

--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,4 @@ export const LOGOUT_MODAL_ID = "my_logout_modal";
 export const appName = "QuickType";
 export const socialMediaHandle = "Mohim Khan";
 export const socialProfileUrl = "https://www.facebook.com/mohim.khan.5076798/";
+export const DB_NAME = "ai_agent";
