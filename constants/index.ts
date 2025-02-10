58c1d7b6c6241fbbf0456a4dc19e263255870f9b
Mohimkhan <mohimkhan652@gmail.com>
2025-02-10 20:09:14 +0600

    fix: update application name from QuickType to TweHub

--- constants/index.ts
+++ constants/index.ts
@@ -1,5 +1,5 @@
 export const LOGOUT_MODAL_ID = "my_logout_modal";
-export const appName = "QuickType";
+export const appName = "TweHub";
 export const socialMediaHandle = "Mohim Khan";
 export const socialProfileUrl = "https://www.facebook.com/mohim.khan.5076798/";
 export const DB_NAME = "ai_agent";
