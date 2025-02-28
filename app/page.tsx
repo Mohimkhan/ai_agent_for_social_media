9e56a78324e61bc9483d8381260527973e095639
Mohimkhan <mohimkhan652@gmail.com>
2025-02-28 07:20:01 +0600

    feat(Home): enhance component to accept username from searchParams and display corresponding GitHub repositories

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import GithubRepos from "@/components/GithubRepos";
+import GithubReposWrapper from "@/components/GithubReposWrapper";
 import { Suspense } from "react";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { username?: string };
+}) {
+  const username = searchParams.username || ""; // Get username from URL
+
   return (
     <div>
-      <Suspense fallback={<p>Loading github repos...</p>}>
-        <GithubRepos />
+      <GithubReposWrapper />
+      <br />
+      <Suspense fallback={<p>Loading github repos of {username}...</p>}>
+        {username && <GithubRepos username={username} />}
       </Suspense>
     </div>
   );
