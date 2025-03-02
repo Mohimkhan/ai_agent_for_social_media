0f8a2e4991ca489497c0b3bc80ec7654d358e407
Mohimkhan <mohimkhan652@gmail.com>
2025-03-02 19:41:58 +0600

    feat(LoginPage): add login page component with signup form integration

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import SignupFormDemo from "@/components/signup-form-demo";
+
+const LoginPage = () => {
+  return (
+    <div className="absolute inset-0 left-1/2 top-1/2 -translate-x-2/4 -translate-y-2/4 w-full px-[10px] sm:px-0">
+      <SignupFormDemo />
+    </div>
+  );
+};
+export default LoginPage;
